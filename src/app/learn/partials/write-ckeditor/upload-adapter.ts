5c08783d2f1097ab1197420e238394bff51ff26f
Poralia <poraliaglobal@gmail.com>
2025-02-18 11:20:56 +0700

    upload image

---
+++ src/app/learn/partials/write-ckeditor/upload-adapter.ts
@@ -0,0 +1,49 @@
+import { inject } from "@angular/core";
+import { Auth, getAuth } from "@angular/fire/auth";
+import { Timestamp } from "@angular/fire/firestore";
+import { getDownloadURL, getStorage, ref, Storage, uploadBytes } from "@angular/fire/storage";
+import { FileLoader, UploadAdapter, UploadResponse } from "ckeditor5/src/upload.js";
+
+type DomFileReader = globalThis.FileReader;
+
+
+export class XAdapter implements UploadAdapter {
+
+    /**
+	 * `FileLoader` instance to use during the upload.
+	 */
+	public loader: FileLoader;
+
+	public reader?: DomFileReader;
+
+	/**
+	 * Creates a new adapter instance.
+	 */
+	constructor( loader: FileLoader ) {
+		this.loader = loader;
+	}
+
+    public upload(): Promise<UploadResponse> {
+        const storage = getStorage();
+        const auth = getAuth();
+
+        return this.loader.file
+			.then( file => new Promise( ( resolve, reject ) => {
+                // 'file' comes from the Blob or File API
+				console.log(file);
+
+                if (file) {
+                    const timestamp = Timestamp.now().toMillis();
+                    const storageRef = ref(storage, `${auth.currentUser?.uid}/learns/${timestamp}-${file.name}`);
+                    uploadBytes(storageRef, file).then(async (snapshot) => {
+                        console.log('Uploaded a blob or file!');
+                        const downloadURL = await getDownloadURL(snapshot.ref);
+                        resolve({
+                            default: downloadURL,
+                        });
+                    });
+                }
+			} ) );
+    }
+
+}
--- src/app/learn/partials/write-ckeditor/write-ckeditor.component.ts
+++ src/app/learn/partials/write-ckeditor/write-ckeditor.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { afterNextRender, afterRender, Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+import { afterNextRender, afterRender, Component, EventEmitter, inject, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { ChangeEvent, CKEditorModule } from '@ckeditor/ckeditor5-angular';
-import { Bold, CKFinder, CKFinderUI, CKFinderUploadAdapter, ClassicEditor, EditorConfig, Essentials, ImageInsertUI, Image as CKImage, ImageResize, ImageStyle, ImageToolbar, ImageUpload, Italic, Link, MediaEmbed, Paragraph, SimpleUploadAdapter, FontColor, FontFamily, Font, FontSize, List, TodoList, TodoListUI, TodoListEditing, Underline, UnderlineEditing, UnderlineUI } from 'ckeditor5';
+import { Bold, CKFinder, CKFinderUI, CKFinderUploadAdapter, ClassicEditor, EditorConfig, Essentials, ImageInsertUI, Image as CKImage, ImageResize, ImageStyle, ImageToolbar, ImageUpload, Italic, Link, MediaEmbed, Paragraph, SimpleUploadAdapter, FontColor, FontFamily, Font, FontSize, List, TodoList, TodoListUI, TodoListEditing, Underline, UnderlineEditing, UnderlineUI, UploadAdapter, UploadResponse, FileLoader, FileRepository, Base64UploadAdapter, Editor } from 'ckeditor5';
 import { debounceTime, Subject } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { UserService } from '../../../auth/services/user/user.service';
+import { XAdapter } from './upload-adapter';
+
 
 @Component({
   selector: 'write-ckeditor',
@@ -15,69 +18,76 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     CKEditorModule,
   ],
 })
-export class WriteCkeditorComponent {
+export class WriteCkeditorComponent implements OnInit {
   
+  private readonly userService = inject(UserService);
   @Output() public onChange: EventEmitter<any> = new EventEmitter<any>;
   private debouncer: Subject<string> = new Subject<string>();
+  private accessToken: string = '';
 
   public data: any;
-  public Editor = ClassicEditor;
-  public config: EditorConfig = {
-    licenseKey: 'GPL', // Or 'GPL'.
-    placeholder: 'Use #hashtags to define a topic.',
-    plugins: [ 
-      Essentials, 
-      Paragraph, 
-      Bold, 
-      Link,
-      Italic,
-      Font,
-      FontColor,
-      FontFamily,
-      FontSize,
-      CKImage,
-      CKFinder,
-      CKFinderUI,
-      CKFinderUploadAdapter,
-      ImageUpload,
-      ImageInsertUI, 
-      SimpleUploadAdapter,
-      List,
-      TodoList,
-      TodoListUI,
-      TodoListEditing,
-      Underline,
-      UnderlineEditing,
-      UnderlineUI,
-    ],
-    toolbar: {
-      items: [ 
-        'undo', 'redo',
-        'fontsize', 'fontColor',
-        'bold', 'underline',
-        'numberedList', 'bulletedList', 'todoList',
-        'imageUpload'
-      ],
-    },
-    simpleUpload: {
-      // The URL that the images are uploaded to.
-      uploadUrl: 'http://example.com',
-
-      // Enable the XMLHttpRequest.withCredentials property.
-      withCredentials: true,
-
-      // Headers sent along with the XMLHttpRequest to the upload server.
-      headers: {
-        'X-CSRF-TOKEN': 'CSRF-Token',
-        Authorization: 'Bearer <JSON Web Token>'
-      }
-    }
-  }
+  public editor = ClassicEditor;
+  public config!: EditorConfig;
 
   constructor() {
     this.debouncer
       .pipe(debounceTime(500), takeUntilDestroyed())
       .subscribe(value => this.onChange.emit(value));
+  }
+
+  async ngOnInit() {
+    const user = await this.userService.getUser();
+    this.accessToken = user.tokenResult.token;
+
+    this.config = {
+      licenseKey: 'GPL', // Or 'GPL'.
+      placeholder: 'Use #hashtags to define a topic.',
+      plugins: [ 
+        Essentials, 
+        Paragraph, 
+        Bold, 
+        Link,
+        Italic,
+        Font,
+        FontColor,
+        FontFamily,
+        FontSize,
+        CKImage,
+        CKFinder,
+        CKFinderUI,
+        CKFinderUploadAdapter,
+        ImageUpload,
+        ImageInsertUI,
+        List,
+        TodoList,
+        TodoListUI,
+        TodoListEditing,
+        Underline,
+        UnderlineEditing,
+        UnderlineUI,
+      ],
+      toolbar: {
+        items: [ 
+          'undo', 'redo',
+          'fontsize', 'fontColor',
+          'bold', 'underline',
+          'numberedList', 'bulletedList', 'todoList',
+          'imageUpload'
+        ],
+      },
+      simpleUpload: {
+        // The URL that the images are uploaded to.
+        uploadUrl: 'https://storage.googleapis.com/upload/storage/v1/b/wdylt-images/o',
+
+        // Enable the XMLHttpRequest.withCredentials property.
+        withCredentials: true,
+
+        // Headers sent along with the XMLHttpRequest to the upload server.
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`
+        }
+      }
+    }
   }
 
   /**
@@ -96,7 +106,14 @@ export class WriteCkeditorComponent {
   public setContent(content: any) {
     this.data = content;
   }
-  
+
+  /**
+   * Editor ready
+   */
+  editorReadyHandler(editor: Editor) {
+    editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+      return new XAdapter( loader );
+    };
+  }
+
 }
-
-
